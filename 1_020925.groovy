bd9dfe137c8886a0f0175b7fedf464f11aa8f91d
JehnJim17 <jehnijimenez@gmail.com>
2026-01-14 15:29:05 -0500

    Update 1_020925.groovy

--- 1_020925.groovy
+++ 1_020925.groovy
@@ -1,3 +1,7 @@
+Charlie yo no quiero pasr por grosera ni nada de esto, se que hoy he estado como muy reactiva, pero de verdad yo quiero que esto quede aclarado lo mas pronto posible, por que sinceramente, yo deje dos oportunidades laborales por que santiago tenia todo arreglado, y sinceramente fue cosas por las que estaba por salir , ademas sergio ni se imagina lo que dijeron, no nos dejan ni negociar osea, literal es entreguenos y ya... no me parece que el trabajo no solo de 2 personas si no de 6 personas quede reducido a Egos
+
+
+
 import pandas as pd
 import io
 from google.colab import files
